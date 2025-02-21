29474f459bbdb03f265a2a6e732cbbb9b5746c54
Pablo Vargas <49590991+PabloVarg@users.noreply.github.com>
2025-02-20 18:48:16 -0600

    refactor(api): Extract mux from server creation

--- api/server.go
+++ api/server.go
@@ -9,9 +9,11 @@ import (
 )
 
 func NewHTTPServer(l *slog.Logger, addr string, n raft.Node) *http.Server {
+	mux := routes(l, n)
+
 	srv := &http.Server{
 		Addr:         addr,
-		Handler:      routes(l, n),
+		Handler:      mux,
 		ReadTimeout:  10 * time.Second,
 		WriteTimeout: 30 * time.Second,
 		IdleTimeout:  time.Minute,
