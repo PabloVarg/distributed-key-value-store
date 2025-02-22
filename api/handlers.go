38d5731af7b1ee2f66ae6857595822e348faa2dd
Pablo Vargas <49590991+PabloVarg@users.noreply.github.com>
2025-02-21 21:37:48 -0600

    feat(api): Handle put key requests

--- api/handlers.go
+++ api/handlers.go
@@ -2,18 +2,54 @@ package api
 
 import (
 	"context"
+	"errors"
 	"log/slog"
 	"net/http"
 	"time"
 
+	"github.com/go-playground/validator/v10"
+	internalRaft "github.com/pablovarg/distributed-key-value-store/raft"
 	"go.etcd.io/raft/v3"
 )
 
 func NewPutHandler(l *slog.Logger, n raft.Node) http.Handler {
+	type input struct {
+		Key   *string `json:"key" ,validate:"required"`
+		Value []byte  `json:"value" ,validate:"required"`
+	}
+
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		var in input
+		if err := readJSON(l, r, &in); err != nil {
+			badRequest(l, w, err)
+			return
+		}
+
+		v := validator.New(validator.WithRequiredStructEnabled())
+		if err := v.Struct(in); err != nil {
+			var vError validator.ValidationErrors
+
+			switch {
+			case errors.As(err, &vError):
+				unprocessableEntity(l, w, buildErrorsResponse(vError))
+			default:
+				internalError(l, r, w, err)
+			}
+			return
+		}
+
+		a, err := internalRaft.EncodeAction(l, internalRaft.StoreAction{
+			Action: internalRaft.Put,
+			Key:    *in.Key,
+			Value:  in.Value,
+		})
+		if err != nil {
+			internalError(l, r, w, err)
+			return
+		}
+
 		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 		defer cancel()
-
-		n.Propose(ctx, []byte("hello"))
+		n.Propose(ctx, a)
 	})
 }
