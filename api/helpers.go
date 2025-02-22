a11033357b514825874498c06556d4acdfef476e
Pablo Vargas <49590991+PabloVarg@users.noreply.github.com>
2025-02-21 21:36:47 -0600

    feat(api): Build response helpers for http

---
+++ api/helpers.go
@@ -0,0 +1,27 @@
+package api
+
+import (
+	"log/slog"
+	"net/http"
+)
+
+func internalError(l *slog.Logger, r *http.Request, w http.ResponseWriter, err error) {
+	l.Error("http internal error", "path", r.URL.String(), "method", r.Method, "error", err.Error())
+	w.WriteHeader(http.StatusInternalServerError)
+}
+
+func badRequest(l *slog.Logger, w http.ResponseWriter, err error) {
+	res := map[string]any{
+		"error": err.Error(),
+	}
+
+	writeJSON(l, res, w, http.StatusBadRequest)
+}
+
+func unprocessableEntity(
+	l *slog.Logger,
+	w http.ResponseWriter,
+	v validationResponse,
+) {
+	writeJSON(l, v, w, http.StatusUnprocessableEntity)
+}
