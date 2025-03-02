2c15bfa115fb35081cb38298787c66d274e0c2f6
Pablo Vargas <49590991+PabloVarg@users.noreply.github.com>
2025-03-02 13:26:41 -0600

    refactor(api): Return 201 on put action

--- api/handlers.go
+++ api/handlers.go
@@ -17,7 +17,7 @@ import (
 // @description inserts or updates a key's value
 // @accept json
 // @param input body api.NewPutHandler.input true "Key / Value pair"
-// @success 200
+// @success 201
 // @router /values [post]
 func NewPutHandler(l *slog.Logger, n raft.Node) http.Handler {
 	type input struct {
@@ -58,6 +58,8 @@ func NewPutHandler(l *slog.Logger, n raft.Node) http.Handler {
 		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 		defer cancel()
 		n.Propose(ctx, a)
+
+		w.WriteHeader(http.StatusCreated)
 	})
 }
 
