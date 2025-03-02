2d24f7214e2c3f5ca838ffc8be192d9d607676c6
Pablo Vargas <49590991+PabloVarg@users.noreply.github.com>
2025-03-02 13:27:02 -0600

    feat(api): Handle raft propose errors

--- api/handlers.go
+++ api/handlers.go
@@ -57,7 +57,11 @@ func NewPutHandler(l *slog.Logger, n raft.Node) http.Handler {
 
 		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 		defer cancel()
-		n.Propose(ctx, a)
+
+		if err := n.Propose(ctx, a); err != nil {
+			internalError(l, r, w, err)
+			return
+		}
 
 		w.WriteHeader(http.StatusCreated)
 	})
